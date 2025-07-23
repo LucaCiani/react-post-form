2e23252c9089818d8f99c1837b85ef8898b8b7f6
LucaCiani <lucaciani88@gmail.com>
2025-07-23 18:59:52 +0200

    add form control

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,90 @@
 import { useState } from "react";
 
 function App() {
+    const [post, setPost] = useState({
+        title: "",
+        author: "",
+        body: "",
+        public: false,
+    });
+
+    const handlePostChange = (e) => {
+        const newValue =
+            e.target.type == "checkbox" ? e.target.checked : e.target.value;
+        setPost({
+            ...post,
+            [e.target.name]: newValue,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        if (!post.author.length || !post.body.length || !post.title.length) {
+            alert("Type all data");
+            return;
+        }
+        e.preventDefault();
+        console.log(post);
+    };
+
     return (
         <>
             <div className="container my-5">
                 <div className="card p-3">
-                    <form className="row">
-                        <div class="col-6 mb-3">
-                            <label for="author" className="form-label">
+                    <form onSubmit={handleSubmit} className="row">
+                        <div className="col-6 mb-3">
+                            <label htmlFor="author" className="form-label">
                                 Author
                             </label>
                             <textarea
+                                onChange={handlePostChange}
                                 className="form-control"
                                 name="author"
                                 id="author"
                                 placeholder="Type the author name"
                             ></textarea>
                         </div>
-                        <div class="col-6 mb-3">
-                            <label for="title" className="form-label">
+                        <div className="col-6 mb-3">
+                            <label htmlFor="title" className="form-label">
                                 Title
                             </label>
                             <textarea
+                                onChange={handlePostChange}
                                 className="form-control"
                                 name="title"
                                 id="title"
                                 placeholder="Type the post title"
                             ></textarea>
                         </div>
-                        <div class="col-12 mb-3">
-                            <label for="body" className="form-label">
+                        <div className="col-12 mb-3">
+                            <label htmlFor="body" className="form-label">
                                 Body
                             </label>
                             <textarea
+                                onChange={handlePostChange}
                                 className="form-control"
                                 name="body"
                                 id="body"
                                 placeholder="Type the post body"
                             ></textarea>
                         </div>
-                        <div class="col-12">
+                        <div className="col-12">
                             <input
-                                class="form-check-input"
+                                onChange={handlePostChange}
+                                className="form-check-input"
+                                name="public"
                                 type="checkbox"
                                 value="public"
                                 id="public"
                             />
-                            <label class="form-check-label" for="public">
+                            <label
+                                className="form-check-label"
+                                htmlFor="public"
+                            >
                                 Public
                             </label>
                         </div>
                         <div className="col-12">
-                            <button
-                                className="btn btn-primary mt-3"
-                                type="button"
-                            >
+                            <button className="btn btn-primary mt-3">
                                 Submit
                             </button>
                         </div>
